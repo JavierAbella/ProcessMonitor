0054c6a8613e2e06501fb46304ecd8cbbc217aa5
javier Abella Mairal <javier.abella@alliance-healthcare.net>
2015-08-21 12:52:10 +0200

    Fix Project libraries

--- ProcessMonitor/src/java/util/Util.java
+++ ProcessMonitor/src/java/util/Util.java
@@ -8,8 +8,6 @@ import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javazoom.jl.decoder.JavaLayerException;
-import javazoom.jl.player.Player;
 
 /**
  *
@@ -25,19 +23,19 @@ public class Util {
     }
 
     private synchronized void playSound(final String file) {
-        new Thread(new Runnable() {
-        // The wrapper thread is unnecessary, unless it blocks on the
-        // Clip finishing; see comments.
-          public void run() {
-            try {        
-            BufferedInputStream bis = new BufferedInputStream(this.getClass().getResourceAsStream("/util/sound/" + file));
-            Player player = new Player(bis);
-            player.play();
-            } catch (JavaLayerException ex) {
-                Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
-            }
-          }
-        }).start();
+//        new Thread(new Runnable() {
+//        // The wrapper thread is unnecessary, unless it blocks on the
+//        // Clip finishing; see comments.
+//          public void run() {
+//            try {        
+//            BufferedInputStream bis = new BufferedInputStream(this.getClass().getResourceAsStream("/util/sound/" + file));
+//            Player player = new Player(bis);
+//            player.play();
+//            } catch (JavaLayerException ex) {
+//                Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//          }
+//        }).start();
     }
      
      public InputStream getProcessProperties(){
